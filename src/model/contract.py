418c4627c5a1cf0b586510fd5f86217f6b456532
VanderSant <vanderson.santos@enline-transmission.com>
2023-03-29 12:31:11 -0300

    🐛 fix json model fields

--- src/model/contract.py
+++ src/model/contract.py
@@ -1,50 +1,49 @@
 from django.db import models
 import uuid
-# from django.core.validators import MaxValueValidator
 
-class StudentData(models.Model):
-    name = models.CharField(max_length=100)
-    full_name = models.CharField(max_length=100)
-    university_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    course = models.CharField(max_length=100)
-    rg = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=100)
-    telephone = models.BigIntegerField()
-    email = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    number = models.IntegerField()
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    cep = models.IntegerField()
-    complement = models.CharField(max_length=100)
-    class_hours = models.IntegerField()    
+# class StudentData(models.Model):
+#     name = models.CharField(max_length=100)
+#     full_name = models.CharField(max_length=100)
+#     university_id = models.UUIDField(default=uuid.uuid4, editable=False)
+#     course = models.CharField(max_length=100)
+#     rg = models.CharField(max_length=100)
+#     cpf = models.CharField(max_length=100)
+#     telephone = models.BigIntegerField()
+#     email = models.CharField(max_length=100)
+#     address = models.CharField(max_length=100)
+#     number = models.IntegerField()
+#     city = models.CharField(max_length=100)
+#     state = models.CharField(max_length=100)
+#     cep = models.IntegerField()
+#     complement = models.CharField(max_length=100)
+#     class_hours = models.IntegerField()    
     
-    def __str__(self):
-        return self.name
-class CompanyData(models.Model):
-    name = models.CharField(max_length=100)
-    cnpj = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    number = models.BigIntegerField()
-    complement = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    cep = models.BigIntegerField()
-    field = models.CharField(max_length=100)
-    telephone = models.BigIntegerField()
-    email = models.CharField(max_length=100)
-    representative = models.CharField(max_length=100)
-    representative_job = models.CharField(max_length=100)
+#     def __str__(self):
+#         return self.name
+# class CompanyData(models.Model):
+#     name = models.CharField(max_length=100)
+#     cnpj = models.CharField(max_length=100)
+#     address = models.CharField(max_length=100)
+#     number = models.BigIntegerField()
+#     complement = models.CharField(max_length=100)
+#     city = models.CharField(max_length=100)
+#     state = models.CharField(max_length=100)
+#     cep = models.BigIntegerField()
+#     field = models.CharField(max_length=100)
+#     telephone = models.BigIntegerField()
+#     email = models.CharField(max_length=100)
+#     representative = models.CharField(max_length=100)
+#     representative_job = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class Contract(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    student_data = models.ForeignKey(StudentData, on_delete=models.CASCADE)
+    student_data = models.JSONField() #models.ForeignKey(StudentData, on_delete=models.CASCADE)
 
-    company_data = models.ForeignKey(CompanyData, on_delete=models.CASCADE)
+    company_data = models.JSONField() #models.ForeignKey(CompanyData, on_delete=models.CASCADE)
 
     duration = models.IntegerField()
     start_date = models.CharField(max_length=100)
--- src/model/report.py
+++ src/model/report.py
@@ -2,11 +2,11 @@ from django.db import models
 import uuid
 # from django.core.validators import MaxValueValidator
 
-class ContractData(models.Model):
-    start_date = models.CharField(max_length=100)
-    end_date = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
+# class ContractData(models.Model):
+#     start_date = models.CharField(max_length=100)
+#     end_date = models.CharField(max_length=100)
+#     def __str__(self):
+#         return self.name
 class Report(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -18,7 +18,7 @@ class Report(models.Model):
     company_name = models.CharField(max_length=100)
     company_report = models.CharField(max_length=100)
 
-    contract_data = models.ForeignKey(ContractData, on_delete=models.CASCADE)
+    contract_data = models.JSONField() # models.ForeignKey(ContractData, on_delete=models.CASCADE)
     
     company_cnpj = models.CharField(max_length=100)
     grade = models.CharField(max_length=100)
